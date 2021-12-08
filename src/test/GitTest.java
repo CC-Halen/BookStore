05688596a81f37d8ebc7c6b296a9a77004b9d510
cdw <cdw9218@gmail.com>
2021-12-08 10:41:50 +0800

    second idea git

---
+++ src/test/GitTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.Test;
+
+/**
+ * @author: cdw
+ * @date: 2021/12/8 10:39
+ * @description:
+ */
+public class GitTest {
+    @Test
+    public void test1(){
+        System.out.println("Hello git !!!!!!!!!!");
+    }
+}
